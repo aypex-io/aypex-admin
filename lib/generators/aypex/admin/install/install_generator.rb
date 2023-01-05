9f9d44e6c5862767ffdfc3a3cc37fbed3c36c905
Matthew Kennedy <m.kennedy@me.com>
2023-01-05 15:33:50 +0000

    Fix CI

--- lib/generators/aypex/admin/install/install_generator.rb
+++ lib/generators/aypex/admin/install/install_generator.rb
@@ -2,7 +2,24 @@ module Aypex
   module Admin
     module Generators
       class InstallGenerator < Rails::Generators::Base
-        desc "Installs Aypex Admin"
+        class_option :migrate, type: :boolean, default: true, banner: "Migrate the database"
+        class_option :add_migrations, type: :boolean, default: true, banner: "Migrate the database"
+
+        def add_migrations
+          if options[:add_migrations] == true
+            run "bundle exec rake railties:install:migrations FROM=aypex_admin"
+          else
+            puts "Skipping rake railties:install:migrations"
+          end
+        end
+
+        def run_migrations
+          if options[:migrate] == true
+            run "bundle exec rake db:migrate VERBOSE=false"
+          else
+            puts "Skipping rake db:migrate, don't forget to run it!"
+          end
+        end
       end
     end
   end
