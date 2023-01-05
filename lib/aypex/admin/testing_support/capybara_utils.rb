d347637830241d107e99856a8b5f768e8b62f92c
Matthew Kennedy <m.kennedy@me.com>
2023-01-05 18:10:33 +0000

    Use common rake.

--- lib/aypex/admin/testing_support/capybara_utils.rb
+++ lib/aypex/admin/testing_support/capybara_utils.rb
@@ -22,14 +22,6 @@ module Aypex
           end
         end
 
-        # delay in seconds
-        def wait_for_ajax(delay = Capybara.default_max_wait_time)
-          Timeout.timeout(delay) do
-            active = page.evaluate_script('typeof jQuery !== "undefined" && jQuery.active')
-            active = page.evaluate_script('typeof jQuery !== "undefined" && jQuery.active') until active.nil? || active.zero?
-          end
-        end
-
         def wait_for_turbo(timeout = nil)
           if has_css?(".turbo-progress-bar", visible: true, wait: 1.seconds)
             has_no_css?(".turbo-progress-bar", wait: timeout.presence || 5.seconds)
