a34b96d04db95fb12f9dcd2e34ea94a398400a7b
Matthew Kennedy <m.kennedy@me.com>
2023-01-14 10:12:56 +0000

    Rename.

--- lib/aypex/admin.rb
+++ lib/aypex/admin.rb
@@ -1,5 +1,6 @@
 require "aypex"
 require "aypex/api"
+require "aypex/admin/engine"
 
 require "turbo-rails"
 require "inline_svg"
@@ -10,8 +11,20 @@ require "aypex/admin/action_callbacks"
 require "aypex/admin/callbacks"
 
 module Aypex
+  def self.admin_path
+    Aypex::Admin::Config.admin_path
+  end
+
+  # Used to configure admin_path for Aypex
+  #
+  # Example:
+  #
+  # write the following line in `config/initializers/aypex.rb`
+  #   Aypex.admin_path = '/custom-path'
+  def self.admin_path=(path)
+    Aypex::Admin::Config.admin_path = path
+  end
+
   module Admin
   end
 end
-
-require "aypex/admin/engine"
