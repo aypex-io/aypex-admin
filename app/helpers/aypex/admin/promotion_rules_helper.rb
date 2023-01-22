30d89a174c2de84ef8d6c3d892adeb25503d17ec
Matthew Kennedy <m.kennedy@me.com>
2023-01-22 18:04:17 +0000

    Flatten I18n

--- app/helpers/aypex/admin/promotion_rules_helper.rb
+++ app/helpers/aypex/admin/promotion_rules_helper.rb
@@ -4,7 +4,7 @@ module Aypex
       def options_for_promotion_rule_types(promotion)
         existing = promotion.rules.map { |rule| rule.class.name }
         rule_names = Rails.application.config.aypex.promotions.rules.map(&:name).reject { |r| existing.include? r }
-        options = rule_names.map { |name| [I18n.t("aypex.admin.promotion_rule_types.#{name.demodulize.underscore}.name"), name] }
+        options = rule_names.map { |name| [I18n.t("aypex.admin.#{name.demodulize.underscore}"), name] }
         options_for_select(options)
       end
     end
